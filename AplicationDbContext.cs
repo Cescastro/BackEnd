af53dbe8564e0120f1e4bb2ce642e1cb60381395
ccastro <ccastro@magnum.com.co>
2022-05-22 18:23:45 -0500

    Agregar archivos de proyecto.

---
+++ AplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd
+{
+    public class AplicationDbContext: DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<TarjetaCredito> TarjetaCreditos { get; set; }
+    }
+}
